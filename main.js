42849c0c84c10a01823cc20e651d2a2b9edb4005
Thunderzapper <justinabossmlg@gmail.com>
2023-04-24 17:32:53 -0400

    more

--- main.js
+++ main.js
@@ -42,9 +42,7 @@ function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 1280,
     height: 720,
-    icon: path.join(__dirname, 'favicon.ico'),
     webPreferences: {
-      preload: path.join(__dirname, './preload.js'), //preload shit
       plugins: true //this is for flash
     }
   })
